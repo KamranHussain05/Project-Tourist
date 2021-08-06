31973a3caa53cc6117b651dc89b58764309bd830
Cpun077 <70039430+Cpun077@users.noreply.github.com>
2021-08-06 16:55:12 -0700

    Creates a window

---
+++ ProjectTourist/src/DrawingSurface.java
@@ -0,0 +1,23 @@
+
+
+import java.awt.Point;
+import java.util.ArrayList;
+import processing.core.PApplet;
+import processing.core.PImage;
+
+public class DrawingSurface extends PApplet{
+	
+	public DrawingSurface() {
+		
+	}
+	
+	public void setup() {
+
+	}
+	
+
+	public void draw() {
+
+	}
+
+}
--- ProjectTourist/src/Main.java
+++ ProjectTourist/src/Main.java
@@ -1,8 +1,28 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+import processing.awt.PSurfaceAWT;
+import processing.core.PApplet;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		DrawingSurface drawing = new DrawingSurface();
+		PApplet.runSketch(new String[]{""}, drawing);
+		PSurfaceAWT surf = (PSurfaceAWT) drawing.getSurface();
+		PSurfaceAWT.SmoothCanvas canvas = (PSurfaceAWT.SmoothCanvas) surf.getNative();
+		JFrame window = (JFrame)canvas.getFrame();
+
+		window.setSize(800, 600);
+		window.setMinimumSize(new Dimension(100,100));
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(true);
+
+		window.setVisible(true);
+		
+		
+		canvas.requestFocus();
 
 	}
 
