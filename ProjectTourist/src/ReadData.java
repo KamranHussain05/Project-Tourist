f81bb4db89ed56c2f49827611fa979c162b87006
Ameya Gandhi <69989742+ameyagandhi@users.noreply.github.com>
2021-08-06 20:20:47 -0700

    basic classes

---
+++ ProjectTourist/src/ReadData.java
@@ -0,0 +1,40 @@
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ReadData {
+	public void readData(String filename, boolean[][] gameData) {
+		File dataFile = new File(filename);
+
+		if (dataFile.exists()) {
+			int count = 0;
+
+			FileReader reader = null;
+			Scanner in = null;
+			try {
+				reader = new FileReader(dataFile);
+				in = new Scanner(reader);
+
+				while (in.hasNext()) {
+					String line = in.nextLine();
+					for (int i = 0; i < line.length(); i++)
+						if (count < gameData.length && i < gameData[count].length && line.charAt(i) == '*')
+							gameData[count][i] = true;
+
+					count++;
+				}
+			} catch (IOException ex) {
+				throw new IllegalArgumentException("Data file " + filename + " cannot be read.");
+			} finally {
+				if (in != null)
+					in.close();
+			}
+
+		} else {
+			throw new IllegalArgumentException("Data file " + filename + " does not exist.");
+		}
+	}
+
+}
