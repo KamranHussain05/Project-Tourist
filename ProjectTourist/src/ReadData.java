a925af78eefccf98186f3cb1d244f129d198d6c7
Ameya Gandhi <69989742+ameyagandhi@users.noreply.github.com>
2021-08-07 11:42:06 -0700

    readdata part done

--- ProjectTourist/src/ReadData.java
+++ ProjectTourist/src/ReadData.java
@@ -1,46 +1,38 @@
 
-import java.io.File;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ReadData {
+	HashMap<String,String> map;
+	public ReadData(HashMap<String,String> map) {
+		this.map = map;
+	}
 	public void readData(String filename, boolean[][] gameData) {
-		File dataFile = new File(filename);
-
-		if (dataFile.exists()) {
-			int count = 0;
-
-			FileReader reader = null;
-			Scanner in = null;
-			try {
-				reader = new FileReader(dataFile);
-				in = new Scanner(reader);
-
-				while (in.hasNext()) {
-					String line = in.nextLine();
-					for (int i = 0; i < line.length(); i++)
-						if (count < gameData.length && i < gameData[count].length && line.charAt(i) == '*')
-							gameData[count][i] = true;
-
-					count++;
-				}
-			} catch (IOException ex) {
-				throw new IllegalArgumentException("Data file " + filename + " cannot be read.");
-			} finally {
-				if (in != null)
-					in.close();
-			}
-
-		} else {
-			throw new IllegalArgumentException("Data file " + filename + " does not exist.");
-		}
+		try(FileWriter fw = new FileWriter("data.txt", true);
+			    BufferedWriter bw = new BufferedWriter(fw);
+			    PrintWriter out = new PrintWriter(bw))
+			{
+			 for (Map.Entry<String, String> entry : map.entrySet()) {
+  
+                // put key and value separated by a colon
+                bw.write(entry.getKey() + ":"
+                         + entry.getValue());
+  
+                // new line
+                bw.newLine();
+            }
+  
+            bw.flush();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
 	}
 	
-	
-	private String getFlightLink(String destination) {
-		return destination;
-		
-	}
 
 }
