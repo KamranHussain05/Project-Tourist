084000dee2004b9a401d1a2f4a7f17eb6eeca81b
Kam <kamran.hssn05@gmail.com>
2021-08-06 17:28:30 -0700

    Update NaturalLanguageProcessing.java

--- ProjectTourist/src/NaturalLanguageProcessing.java
+++ ProjectTourist/src/NaturalLanguageProcessing.java
@@ -1,6 +1,38 @@
+import java.awt.List;
+
 // Author: Kamran Hussain
 // Date: 8/6/21
 // 
 public class NaturalLanguageProcessing {
-
+	
+	public NaturalLanguageProcessing() {
+		
+	}
+	
+	
+	// Accessor methods for file writer and front end
+	public String getDestination(String s) {
+		return s;
+	}
+	
+	public String getActivityOne(String s) {
+		return s;
+	}
+	
+	public String getActivityTwo(String s) {
+		return s;
+	}
+	
+	public String getActivityThree(String s) {
+		return s;
+	}
+	
+	public String getActivityFour(String s) {
+		return s;
+	}
+	
+	public List getProcessedData(String loc, String actOne, String actTwo) {
+		List results = null;
+		return results;
+	}
 }
