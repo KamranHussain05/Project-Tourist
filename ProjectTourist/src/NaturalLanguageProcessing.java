2fec2b9f2abdcf40f37dfa6f11145622080d5a8c
Kam <kamran.hssn05@gmail.com>
2021-08-06 17:42:10 -0700

    Update NaturalLanguageProcessing.java

--- ProjectTourist/src/NaturalLanguageProcessing.java
+++ ProjectTourist/src/NaturalLanguageProcessing.java
@@ -1,9 +1,16 @@
 import java.awt.List;
+import java.util.Dictionary;
 
 // Author: Kamran Hussain
 // Date: 8/6/21
 // 
 public class NaturalLanguageProcessing {
+	private String dest;
+	private String act1;
+	private String act2;
+	private String act3;
+	private String act4;
+	
 	
 	public NaturalLanguageProcessing() {
 		
@@ -11,28 +18,39 @@ public class NaturalLanguageProcessing {
 	
 	
 	// Accessor methods for file writer and front end
-	public String getDestination(String s) {
+	public String getDestination() {
+		String s = dest;
 		return s;
 	}
 	
-	public String getActivityOne(String s) {
+	public String getActivityOne() {
+		String s = act1;
 		return s;
 	}
 	
-	public String getActivityTwo(String s) {
+	public String getActivityTwo() {
+		String s = act2;
 		return s;
 	}
 	
-	public String getActivityThree(String s) {
+	public String getActivityThree() {
+		String s = act3;
 		return s;
 	}
 	
-	public String getActivityFour(String s) {
+	public String getActivityFour() {
+		String s = act4;
 		return s;
 	}
 	
-	public List getProcessedData(String loc, String actOne, String actTwo) {
-		List results = null;
+	@SuppressWarnings("null")
+	public Dictionary<String, String> getProcessedData(String loc, String actOne, String actTwo, String actThree) {
+		Dictionary<String, String> results = null;
+		results.put("location", loc);
+		results.put("activityOne", actOne);
+		results.put("activityTwo", actTwo);
+		results.put("activityThree", getActivityThree());
+		
 		return results;
 	}
 }
