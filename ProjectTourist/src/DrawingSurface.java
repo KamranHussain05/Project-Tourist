fe972ca30c83d8bf83cee57f51977eeb7df75f2e
Cpun077 <70039430+Cpun077@users.noreply.github.com>
2021-08-06 22:07:09 -0700

    Updated Window 2.0
    Added makeshift bar, centered and labelled screen, edited size, etc.

--- ProjectTourist/src/DrawingSurface.java
+++ ProjectTourist/src/DrawingSurface.java
@@ -12,12 +12,19 @@ public class DrawingSurface extends PApplet{
 	}
 	
 	public void setup() {
-
+		surface.setTitle("Project Tourist");
+		surface.setLocation(37, 55);
 	}
 	
 
 	public void draw() {
-
+		this.rect(200, 200, 540, 45, 50);
+		
+		pushStyle();
+		fill(0);
+		textSize(10);
+		text("Interactions- House Move: Arrow Keys, Person Move: WASD, \nHouse Scale: Left/Right Click, Sun move: comma and period", 120, 10);
+		popStyle();
 	}
 
 }
--- ProjectTourist/src/Main.java
+++ ProjectTourist/src/Main.java
@@ -14,8 +14,8 @@ public class Main {
 		PSurfaceAWT.SmoothCanvas canvas = (PSurfaceAWT.SmoothCanvas) surf.getNative();
 		JFrame window = (JFrame)canvas.getFrame();
 
-		window.setSize(800, 600);
-		window.setMinimumSize(new Dimension(100,100));
+		window.setSize(1366, 768);
+		window.setMinimumSize(new Dimension(1366, 768));
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(true);
 
