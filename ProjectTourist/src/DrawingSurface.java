e990c85f7feb40013e68ba55f32114b7f0ceaef9
Cpun077 <70039430+Cpun077@users.noreply.github.com>
2021-08-07 02:21:34 -0700

    Added buttons, made workable search bar
    Main Menu at least is done

--- ProjectTourist/src/DrawingSurface.java
+++ ProjectTourist/src/DrawingSurface.java
@@ -1,35 +1,74 @@
+import java.awt.Font;
 
-
-import java.awt.Point;
-import java.util.ArrayList;
+import g4p_controls.*;
 import processing.core.PApplet;
-import processing.core.PImage;
 
 public class DrawingSurface extends PApplet{
 	
-	private String textBar;
-	
-	public DrawingSurface() {
-		
-	}
+	private boolean randDestination;
+	private boolean randExperience;
+	private GButton randD;
+	private GButton randE;
+	private GButton randAll;
+	private GTextField searchBar;
 	
 	public void setup() {
+		randDestination = false;
+		randExperience = false;
 		surface.setTitle("Project Tourist");
 		surface.setLocation(37, 55);
-		textBar = "Type a Location";
+		randD = new GButton(this, 85, 450, 342, 100, "Random Destination");
+		randD.setLocalColorScheme(1); // http://www.lagers.org.uk/g4p/guides/g04-colorschemes.html
+		randE = new GButton(this, 512, 450, 342, 100, "Random Experience");
+		randE.setLocalColorScheme(1);
+		randAll = new GButton(this, 939, 450, 342, 100, "Random Destination and Experience");
+		randAll.setLocalColorScheme(1);
+		searchBar = new GTextField(this, (float)450, (float)270, (float)466, (float)30);
+		searchBar.setFont(new Font("Sans_Serif", Font.PLAIN, 20));
+		searchBar.setPromptText("Type Your Destination");
 	}
 	
 
 	public void draw() {
-		background(255, 165, 0);
-		this.rect(450, 270, 466, 55, 50);
+		background(195, 225, 210);
+		randD.setVisible(true);
+		randE.setVisible(true);
+		randAll.setVisible(true);
+		searchBar.setVisible(true);
 		this.image(loadImage("res/Logo.png"), 486, 100);
-		
-		pushStyle();
-		fill(0);
-		textSize(20);
-		text(textBar, 480, 305);
-		popStyle();
 	}
-
+	
+	public void keyPressed() {
+		if (key == ENTER || key == RETURN) { //Will exit search bar if ENTER or RETURN is pressed
+			searchBar.setFocus(false);
+		}
+	}
+	
+	public void handleButtonEvents(GButton button, GEvent event) {
+		if (button == randD && event == GEvent.CLICKED) {
+		      randDestination = true;
+		}
+		if (button == randE && event == GEvent.CLICKED) {
+		      randExperience = true;
+		}
+		if (button == randAll && event == GEvent.CLICKED) {
+		      randDestination = true;
+		      randExperience = true;
+		}
+	}
+	
+	public boolean getrandD() {
+		return randDestination;
+	}
+	
+	public boolean getrandE() {
+		return randExperience;
+	}
+	
+	public String getDestination() {
+		if (!randDestination)
+			return searchBar.getText();
+		else 
+			return "RANDOM"; //Choose random destination if this method returns this
+	}
 }
