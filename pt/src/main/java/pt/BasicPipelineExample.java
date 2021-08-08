55653465c31eef35b8b5579aaf790dfe468803af
Kam <kamran.hssn05@gmail.com>
2021-08-07 22:22:36 -0700

    Update BasicPipelineExample.java

--- pt/src/main/java/pt/BasicPipelineExample.java
+++ pt/src/main/java/pt/BasicPipelineExample.java
@@ -11,12 +11,7 @@ import java.util.*;
 
 public class BasicPipelineExample {
 
-  public static String text = "Joe Smith was born in California. " +
-      "In 2017, he went to Paris, France in the summer. " +
-      "His flight left at 3:00pm on July 10th, 2017. " +
-      "After eating some escargot for the first time, Joe said, \"That was delicious!\" " +
-      "He sent a postcard to his sister Jane Smith. " +
-      "After hearing about Joe's trip, Jane decided she might go to France one day.";
+  public static String text = "Biking along the champs elysee in Paris, France.";
 
   public static void main(String[] args) {
     // set up pipeline properties
@@ -33,26 +28,8 @@ public class BasicPipelineExample {
     pipeline.annotate(document);
     // examples
 
-    // 10th token of the document
-    CoreLabel token = document.tokens().get(10);
-    System.out.println("Example: token");
-    System.out.println(token);
-    System.out.println();
-
-    // text of the first sentence
-    String sentenceText = document.sentences().get(0).text();
-    System.out.println("Example: sentence");
-    System.out.println(sentenceText);
-    System.out.println();
-
     // second sentence
     CoreSentence sentence = document.sentences().get(1);
-
-    // list of the part-of-speech tags for the second sentence
-    List<String> posTags = sentence.posTags();
-    System.out.println("Example: pos tags");
-    System.out.println(posTags);
-    System.out.println();
 
     // list of the ner tags for the second sentence
     List<String> nerTags = sentence.nerTags();
@@ -64,12 +41,6 @@ public class BasicPipelineExample {
     Tree constituencyParse = sentence.constituencyParse();
     System.out.println("Example: constituency parse");
     System.out.println(constituencyParse);
-    System.out.println();
-
-    // dependency parse for the second sentence
-    SemanticGraph dependencyParse = sentence.dependencyParse();
-    System.out.println("Example: dependency parse");
-    System.out.println(dependencyParse);
     System.out.println();
 
     // kbp relations found in fifth sentence
@@ -97,24 +68,6 @@ public class BasicPipelineExample {
     Map<Integer, CorefChain> corefChains = document.corefChains();
     System.out.println("Example: coref chains for document");
     System.out.println(corefChains);
-    System.out.println();
-
-    // get quotes in document
-    List<CoreQuote> quotes = document.quotes();
-    CoreQuote quote = quotes.get(0);
-    System.out.println("Example: quote");
-    System.out.println(quote);
-    System.out.println();
-
-    // original speaker of quote
-    // note that quote.speaker() returns an Optional
-    System.out.println("Example: original speaker of quote");
-    System.out.println(quote.speaker().get());
-    System.out.println();
-
-    // canonical speaker of quote
-    System.out.println("Example: canonical speaker of quote");
-    System.out.println(quote.canonicalSpeaker().get());
     System.out.println();
 
   }
