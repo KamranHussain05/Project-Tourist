a715f157c7feafc9b3be85df1a242cfe4a47170f
KamranHussain05 <khussain592@student.fuhsd.org>
2021-09-09 22:30:32 -0700

    Update FileWritingTester.java

--- pt/src/main/java/pt/FileWritingTester.java
+++ pt/src/main/java/pt/FileWritingTester.java
@@ -5,13 +5,13 @@ public class FileWritingTester {
 	private static WriteToFile w;
 	
 	public FileWritingTester() {
-		w = new WriteToFile();
+		//w = new WriteToFile();
 	}
 
 	public static void main(String[] args) {
 		String s = "test";
-		w = new WriteToFile();
 		for (int i =0; i<5; i++) {
+			w = new WriteToFile();
 			w.writeToFile(s+i);
 			System.out.println(i);
 		}
